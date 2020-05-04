03460854afa21eb667dbd2530c6e02b331b48ea8
Nate Hart <nate.hart@joinhoney.com>
2020-05-03 22:37:07 -0700

    socket setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import io from 'socket.io-client';
+
+// TODO: load from env
+const BASE_URL = "http://localhost:4001";
 
 function App() {
+  const [partyId, setPartyId] = useState(null);
+  const [gameState, setGameState] = useState({});
+
+  useEffect(() => {
+    const connect = async () => {
+      const res = await axios.get(BASE_URL);
+      console.log(res);
+    };
+    connect();
+  }, []);
+
+  useEffect(() => {
+    if (partyId) {
+      const socket = io(`${BASE_URL}/${partyId}`);
+      socket.on("connect", () => {
+        console.log("** socket connected")
+      });
+      socket.on("update", setGameState)
+    }
+  }, [partyId]);
+
+  const createParty = useCallback(() => {
+    const makeRequest = async () => {
+      const res = await axios.post("http://localhost:4001/parties");
+      console.log(res)
+  
+      const { data = {} } = res;
+      const { partyId } = data;
+      setPartyId(partyId);
+    }
+
+    makeRequest();
+  });
+
   return (
     <div className="App">
       <h1 style={{ margin: "20px" }}>Charades</h1>
       <h4 style={{ margin: "20px" }}>This is a charades app!</h4>
+      {
+        partyId
+          ? `created party ${partyId}`
+          : <button onClick={ createParty }>Create Party</button>
+      }
+      <br/>
+      { JSON.stringify(gameState)}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,49 @@
-const express = require("express");
-const app = express();
-const port = 4001;
+import http from "http";
+import express from "express";
+import socketIo from "socket.io";
 
+import SocketService from "./services/socket.js";
+import AppManager from "./AppManager.js";
+
+// TODO: load from env
+const PORT = 4001;
+
+/* MARK: initializations */
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const socket = new SocketService(io)
+const manager = new AppManager(socket);
+
+/* MARK: middleware */
+// TODO: break out into separate file, add more middleware:
+//  - add `{ body: {} }` to post that don't have a body
+//  - at some point we'll want sone security middleware
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // TODO: better cors handling
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true")
+  next();
+});
+
+/* MARK: routes */
+// TODO: create separate routes and controllers files
 app.get("/", (req, res) => res.send("everyone is good enough"));
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.post("/parties", (req, res) => {
+  const { hostName = "player1", settings = {} } = req.body || {};
+  const initialState = manager.createParty(hostName, settings);
+  res.status(200).send(initialState);
+});
+
+app.post("/parties/players", (req, res) => {
+  const { playerName = "newplayer", partyId = "0000" } = req.body || {};
+  const newState = manager.addPlayerToParty(playerName, partyId);
+  res.status(200).send(newState);
+});
+
+/* MARK: listen */
+server.listen(PORT, () => {
+  console.log(`Charades server listening on port ${PORT}`);
+});
