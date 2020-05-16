5533ffdd7e36484b0f69f607de5c38dbec79f215
anya solotaire <62819236+bo-banya@users.noreply.github.com>
2020-05-16 15:59:28 -0500

    Midgame test data (#19)
    * midGamPhase test export
    
    * midgame test data
    
    * midgame test data with prompts deleted

--- server/src/tests/testData.js
+++ server/src/tests/testData.js
@@ -62,7 +62,132 @@ export const promptPhase = {
 };
 
 // mid game
-export const midGamePhase = {};
+export const midGamePhase = {
+  slug: "trouble-maker",
+    host: "bobanya",
+    createdAt: 1588553102804,
+    updatedAt: 1588553102804,
+    players: [
+        "bobanya",
+        "genji",
+        "millie",
+        "jilly",
+        "rosa",
+        "heinrich"
+    ],
+    settings: {
+        rotations: 2,
+        turnDurationSeconds: 90,
+        teamsCount: 2, 
+        autoStart: false, 
+    },
+    games: [
+        {
+            startTime: 1588553102804,
+            endTime: 1588553102804,
+                {
+                    teamName: "Booty Shakers",
+                    players: [
+                        "jilly",
+                        "rosa",
+                        "heinrich",
+                    ],
+                    playerIndex: 0,
+                    score: 3,
+                },
+                {
+                  teamName: "Silver Tiger Claw",
+                  players: [
+                      "bobanya",
+                      "genji",
+                      "millie",
+                  ],
+                  playerIndex: 2,
+                  score: 1,
+              },
+            ],
+            totalTurns: 12,
+            turns: [
+                {
+                    startTime: 1588553102804,
+                    endTime: 1588554102804,
+                    teamIndex: 0,
+                    player: "jilly",
+                    author: "bobanya",
+                    prompt: "car jacking",
+                    success: true,
+                },
+                {
+                    startTime: 1588555102804,
+                    endTime: 1588556102804,
+                    teamIndex: 1,
+                    player: "bobanya",
+                    author: "jilly",
+                    prompt: "aquaphor",
+                    success: true,
+                },
+                {
+                    startTime: 1588557102804,
+                    endTime: 1588558102804,
+                    teamIndex: 0,
+                    player: "rosa",
+                    author: "genji",
+                    prompt: "snail eating a cucumber",
+                    success: true,
+                },
+                {
+                    startTime: 1588558102804,
+                    endTime: 1588559102804,
+                    teamIndex: 1,
+                    player: "genji",
+                    author: "rosa",
+                    prompt: "five star wings",
+                    success: false,
+                },
+                {
+                    startTime: 1588558102804,
+                    endTime: 1588559102804,
+                    teamIndex: 0,
+                    player: "heinrich",
+                    author: "millie",
+                    prompt: "i'm a lil bitch",
+                    success: true,
+                }
+            ]
+        }
+    ],
+    prompts: [
+        {
+            author: "bobanya",
+            prompt: "lip light"
+        },
+        {
+            author: "genji",
+            prompt: "star wards: the last jedi"
+        },
+        {
+            author: "millie",
+            prompt: "plush baby legs"
+        },
+        {
+            author: "jilly",
+            prompt: "smoking is cool kids"
+        },
+        {
+            author: "rosa",
+            prompt: "smoothie hoochie"
+        },
+        {
+            author: "heinrich",
+            prompt: "gloop gloop gloop"
+        },
+        {
+            author: "heinrich",
+            prompt: "mousie go bye bye"
+        }
+    ]
+
+};
 
 // one turn left in game
 export const endGamePhase = {};
