d82f8e52b1deaa5103501da14d918baa33337b27
Anders Pollack <pollack.anders@gmail.com>
2020-05-21 21:18:51 -0700

    Clean up Landing Page; Button and TextInput styles (#26)
    * Clean up Button and TextInput styles and props
    
    - Allow style props to pass through
    - Make TextInput match design
    - Add focus styles to buttons and inputs
    
    * Reorder component styles
    
    * Give inputs and buttons better defaults

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,14 +2,17 @@ import React from "react";
 
 const Button = (props) => {
   const classes = `button
-${props.className ? props.className : ''}
-${props.type === 'primary' && 'button-primary text__heading'} 
-${props.type === 'secondary' && 'button-secondary'} 
-${props.disabled && 'button-disabled'}
-  `;
+  ${props.className ? props.className : ''}
+  ${props.type === 'primary' && 'button-primary text__heading'}
+  ${props.type === 'secondary' && 'button-secondary'}
+  ${props.disabled && 'button-disabled'}`;
 
   return (
-    <button className={classes} onClick={props.onClick}>
+    <button
+      className={classes}
+      onClick={props.onClick}
+      style={props.style}
+    >
       {props.icon && (
         <span className="button-secondary__icon">{props.icon}</span>
       )}
