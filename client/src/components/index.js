4f0504063e2aeee0b9ed1af044c861aac6fd16ad
Anders Pollack <pollack.anders@gmail.com>
2020-05-25 09:08:12 -0700

    Adding dummy TeamBox styles and markup to TeamBox component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import TextInput from "./TextInput";
 import Button from "./Button";
 import CloseButton from "./CloseButton";
+import TeamBox from "./TeamBox";
 
-export { TextInput, Button, CloseButton };
+export { TextInput, Button, CloseButton, TeamBox };
