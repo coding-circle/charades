d1a28649b2e3da308ce15ea5819a77054c4c419a
Anders Pollack <pollack.anders@gmail.com>
2020-05-28 23:50:11 -0700

    Fix indentation

--- client/src/views/Sandbox.jsx
+++ client/src/views/Sandbox.jsx
@@ -4,17 +4,17 @@ import { TeamBox, PlayerList, Score } from '../components';
 function Sandbox() {
   return (
     <div className="app__main app__main--home">
-        <TeamBox
-          teamName={'Christmas Santas'}
-          myTeam={true}
+      <TeamBox
+        teamName={'Christmas Santas'}
+        myTeam={true}
+        color={'aquamarine'}
+      >
+        <PlayerList
+          players={['Fortch', 'Stung', 'Pristina']}
+          host={'Stung'}
+          actorUp={'Fortch'}
+          onDeck={'Pristina'}
           color={'aquamarine'}
-        >
-          <PlayerList
-            players={['Fortch', 'Stung', 'Pristina']}
-            host={'Stung'}
-            actorUp={'Fortch'}
-            onDeck={'Pristina'}
-            color={'aquamarine'}
           />
           <Score score={48} />
         </TeamBox>
