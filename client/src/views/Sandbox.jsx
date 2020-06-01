89bcdc73ef2c258fce07eb89684c2c40e190eb2d
Anders Pollack <pollack.anders@gmail.com>
2020-05-31 22:07:43 -0700

    Add Modal example to Sandbox

--- client/src/views/Sandbox.jsx
+++ client/src/views/Sandbox.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { TeamBox, PlayerList, Score } from '../components';
+import React, { useState } from "react";
+import { TeamBox, PlayerList, Score, Button, Modal, TextInput } from '../components';
 
 function Sandbox() {
+  const [active, setActive] = useState(false);
+
   return (
     <div className="app__main app__main--home">
       <TeamBox
@@ -18,6 +20,16 @@ function Sandbox() {
           />
         <Score score={48} />
       </TeamBox>
+      <Button onClick={() => setActive(true)}>open modal</Button>
+      <Modal
+        title="I Wish U Would?"
+        isActive={active}
+        type="alert"
+        submitButtonText="Wow ok!"
+      >
+        <p style={{marginBottom: '12px'}}>Sup with all this wild stuff</p>
+        <TextInput/>
+      </Modal>
     </div>
   );
 }
