115f0858b2dbcd23c1424819aa09af27b428b9d5
Andy Scheff <andyscheff@gmail.com>
2020-06-07 15:45:44 -0700

    Updates

--- server/src/db/methods/index.js
+++
@@ -1,6 +0,0 @@
-import devMethods from "./dev";
-import gameMethods from "./game";
-import helpers from "./helpers";
-import partyMethods from "./party";
-
-export { devMethods, gameMethods, helpers, partyMethods };
