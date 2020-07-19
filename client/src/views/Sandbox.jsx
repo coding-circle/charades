7cd7adf6290628268e2c8a538381e890821917ff
Jack Vaught <johnmvaught@gmail.com>
2020-07-18 19:58:26 -0700

    fixes sandbox error

--- client/src/views/Sandbox.jsx
+++ client/src/views/Sandbox.jsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { PreviousTurnBox } from "../components";
 
 function Sandbox() {
-  const { countdown, percentage } = useTimer({
-    startTime: 1593982941592,
-    turnDurationSeconds: 90,
-  });
-
-  return (
-    <div className="app_main">
-      <h1> PreviousTurnBox </h1>
-      <PreviousTurnBox
-        player="george"
-        teamColor="red"
-        success={false}
-        prompt="fig tree"
-      />
-    </div>
-  );
+  return <div className="app_main"></div>;
 }
 
 export default Sandbox;
