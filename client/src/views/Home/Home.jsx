faefa8073b194d69e097546cc0771c09f0f86e01
James Sheppard <63818299+jgsheppa@users.noreply.github.com>
2020-12-19 20:43:00 +0100

    Add HowToPlay component to Home page

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 
 import CreateGame from "./CreateParty";
 import JoinParty from "./JoinParty";
+import HowToPlay from "./HowToPlay";
 import { Modal } from "../../components";
 
 function Home({ username: storageUsername, slug, setCurrentViewToParty }) {
   const [createGameOpen, setCreateGameOpen] = useState(false);
+  const [howToPlayOpen, setHowToPlayOpen] = useState(false);
   const [username, setUsername] = useState(storageUsername.slice(0, -7));
   const [errorMessage, setErrorMessage] = useState("");
 
   const showCreateGameView = () => setCreateGameOpen(true);
   const hideCreateGameView = () => setCreateGameOpen(false);
 
+  // Show and hide content about how to play with charades app
+  const showHowToPlay = () => setHowToPlayOpen(true);
+  const hideHowToPlay = () => setHowToPlayOpen(false);
+
   const handleErrorModalClose = () => setErrorMessage("");
 
   return (
     <div id="app">
-      {createGameOpen ? (
+      {createGameOpen && (
         <CreateGame
           username={username}
           setUsername={setUsername}
@@ -24,12 +30,15 @@ function Home({ username: storageUsername, slug, setCurrentViewToParty }) {
           setCurrentViewToParty={setCurrentViewToParty}
           setErrorMessage={setErrorMessage}
         />
-      ) : (
+      )}
+      {howToPlayOpen && <HowToPlay hideHowToPlay={hideHowToPlay} />}
+      {!createGameOpen && !howToPlayOpen && (
         <JoinParty
           slug={slug}
           username={username}
           setUsername={setUsername}
           showCreateGameView={showCreateGameView}
+          showHowToPlay={showHowToPlay}
           setCurrentViewToParty={setCurrentViewToParty}
           setErrorMessage={setErrorMessage}
         />
