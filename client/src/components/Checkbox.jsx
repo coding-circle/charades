9f3ac773be6ce6c023821523db1590e65e320f4b
Diego Glusberg <chedgo@gmail.com>
2021-01-14 15:18:18 -0600

    restyled checkbox component, added labelBefore variant (#96)
    * restyled checkbox component, added labelBefore variant
    
    * fixed logic for type coercion and placement of label

--- client/src/components/Checkbox.jsx
+++ client/src/components/Checkbox.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Checkbox.css";
 
-function Checkbox({ label, checked, value, onChange }) {
-  const handleChange = () => onChange(!checked);
-
+function Checkbox({ label, checked, value, onChange, labelBefore }) {
+  const [isChecked, setChecked] = useState(!!checked);
+  const handleChange = () => {
+    const checkedState = !isChecked;
+    setChecked(checkedState);
+    onChange(checkedState);
+  };
+  const baseClass = "checkbox";
+  const wrapperClass = labelBefore
+    ? `${baseClass} ${baseClass}--label-before`
+    : baseClass;
   return (
-    <span className="checkbox" onClick={handleChange}>
+    <span className={wrapperClass} onClick={handleChange}>
       <input
         className="checkbox__input"
         type="checkbox"
         name={`${label}-checkbox`}
         value={value}
-        checked={checked}
-        onChange={handleChange}
+        checked={isChecked}
       />
-      <label htmlFor={`${label}-checkbox`}>{label}</label>
+      <div className="checkbox__overlay">
+        <span className="checkbox__check">&#10003;</span>
+      </div>
+      <label className="checkbox__label" htmlFor={`${label}-checkbox`}>{label}</label>
     </span>
   );
 }
